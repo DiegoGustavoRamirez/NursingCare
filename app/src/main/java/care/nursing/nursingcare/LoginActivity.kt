9a39b84bbad37c20c4870caf080cce4d03ffae65
DiegoGustavoRamirez <diegormz111@gmail.com>
2019-01-19 16:35:10 -0600

    ---

--- app/src/main/java/care/nursing/nursingcare/LoginActivity.kt
+++ app/src/main/java/care/nursing/nursingcare/LoginActivity.kt
@@ -1,12 +1,44 @@
 package care.nursing.nursingcare
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val buttonLogin: Button = findViewById(R.id.button_login)
+        buttonLogin.setOnClickListener { listenerLogin() }
+
     }
+
+    private fun listenerLogin (){
+        val etUserLogin :EditText = findViewById(R.id.et_user_login)
+        val etPasswordLogin : EditText = findViewById(R.id.et_password_login)
+        val userName = etUserLogin.text.toString()
+
+        if (userName == getString(R.string.username_login)
+            && etPasswordLogin.text.toString() == getString(R.string.password_login) ) {
+
+            val sharedPreferencesFile = this.getSharedPreferences(getString(R.string.variable_name_shared_preferences),0)
+            val sharedPreferencesEditor = sharedPreferencesFile!!.edit()
+            sharedPreferencesEditor.putString(getString(R.string.variable_name_username),userName)
+            sharedPreferencesEditor.apply()
+
+            val intentMainActivity = Intent(this, MainActivity::class.java)
+            startActivity(intentMainActivity)
+            this.finish()
+        }
+        else{
+            Toast.makeText(this, getString(R.string.toast_error_login_message), Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
+
+
--- app/src/main/java/care/nursing/nursingcare/MainActivity.kt
+++ app/src/main/java/care/nursing/nursingcare/MainActivity.kt
@@ -1,12 +1,95 @@
 package care.nursing.nursingcare
 
-import android.support.v7.app.AppCompatActivity
+import android.app.FragmentManager
+import android.app.ListFragment
+import android.net.Uri
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), HomeFragment.OnFragmentInteractionListener, NavigationView.OnNavigationItemSelectedListener {
+
+
+
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId) {
+            R.id.action_settings -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_home -> {
+                supportFragmentManager.beginTransaction().add(R.id.content_main, HomeFragment()).commit()
+
+            }
+
+            R.id.nav_perfil ->{
+
+            }
+
+            R.id.nav_ganancias ->{
+
+            }
+
+            R.id.nav_guardias ->{
+
+            }
+
+            R.id.nav_pacientes ->{
+
+            }
+
+            R.id.nav_historial ->{
+
+            }
+
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
